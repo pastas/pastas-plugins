5b6eca7fb3b42c4e87a8e3831782fb1ca864ca3b
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-07-11 14:46:56 +0200

    import reservoirs in reservoir plugin

--- pastas_plugins/reservoirs/__init__.py
+++ pastas_plugins/reservoirs/__init__.py
@@ -1,1 +1,4 @@
+# ruff: noqa: F401
+from pastas_plugins.reservoirs.reservoir import Reservoir1, Reservoir2
+from pastas_plugins.reservoirs.stressmodels import ReservoirModel
 from pastas_plugins.reservoirs.version import __version__
