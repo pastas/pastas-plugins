e9dcfc98532b9a0a4301579429d39b966a453dcb
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-07-23 16:18:09 +0200

    add ccf plots - plot_corr: plot correlation (acf or ccf) - plot_ccf_overview: function for visualizing ccf between two time series. - add example to notebook - update init

--- pastas_plugins/crosscorrelation/__init__.py
+++ pastas_plugins/crosscorrelation/__init__.py
@@ -1,3 +1,4 @@
 # ruff: noqa: F401
 from pastas_plugins.crosscorrelation.crosscorr import ccf, fit_response, prewhiten
+from pastas_plugins.crosscorrelation.plots import ccf_overview_plot, plot_corr
 from pastas_plugins.crosscorrelation.version import __version__
