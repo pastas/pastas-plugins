ab09ee047d3fee77df33f05d5b5f5db2a67d16e6
Martin Vonk <vonk.mart@gmail.com>
2024-07-24 09:52:06 +0200

    fix ruff

--- pastas_plugins/cross_correlation/__init__.py
+++ pastas_plugins/cross_correlation/__init__.py
@@ -1,4 +1,8 @@
 # ruff: noqa: F401
-from pastas_plugins.cross_correlation.cross_correlation import ccf, fit_response, prewhiten
+from pastas_plugins.cross_correlation.cross_correlation import (
+    ccf,
+    fit_response,
+    prewhiten,
+)
 from pastas_plugins.cross_correlation.plots import plot_ccf_overview, plot_corr
 from pastas_plugins.cross_correlation.version import __version__
