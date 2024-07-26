98b957075eaa67d374f5a99306ab73b2849499ff
Martin Vonk <vonk.mart@gmail.com>
2024-07-26 10:19:44 +0200

    remove parameters view print statement

--- pastas_plugins/pest/_run_pastas_model.py
+++ pastas_plugins/pest/_run_pastas_model.py
@@ -5,14 +5,17 @@ def run():
     from pandas import read_csv
     from pastas.io.base import load as load_model
 
+    # base path
     fpath = Path(__file__).parent
+
     # load pastas model
     ml = load_model(fpath / "model.pas")
-    # update parameters
+
+    # update model parameters
     parameters = read_csv(fpath / "parameters_sel.csv", index_col=0)
     for pname, val in parameters.loc[:, "optimal"].items():
         ml.set_parameter(pname.replace("_g", "_A"), optimal=val)
-    print("parameters view\n", ml.parameters.loc[:, "optimal"])
+
     # simulate
     simulation = ml.simulate()
     simulation.loc[ml.observations().index].to_csv(fpath / "simulation.csv")
