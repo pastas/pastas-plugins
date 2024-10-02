05b233b2c74fae8b7da7a6600a0a3f607deea455
Martin Vonk <vonk.mart@gmail.com>
2024-10-02 11:57:58 +0200

    start with uzf

--- pastas_plugins/modflow/modflow.py
+++ pastas_plugins/modflow/modflow.py
@@ -1,6 +1,6 @@
 import functools
 import logging
-from typing import List
+from typing import List, Literal
 
 import flopy
 import numpy as np
@@ -187,4 +187,112 @@ class ModflowRch(Modflow):
         rch.write()
         rch.ts.write()
 
+        self._gwf.name_file.write()
+
+
+class ModflowUzf(Modflow):
+    def __init__(
+        self,
+        exe_name: str,
+        sim_ws: str,
+        nlay: int,
+        simulate_et: bool = True,
+        gwet_linear_or_square: Literal["linear", "square"] = "linear",
+        unsat_et_wc_or_ae: Literal["wc", "ae"] = "ae",
+        raise_on_modflow_error: bool = False,
+    ):
+        self._name = "mf_uzf"
+        self._stress = None
+        self._simulation = None
+        self._gwf = None
+        self._changing_packages = ("STO", "GHB", "UZF")
+        Modflow.__init__(self, exe_name, sim_ws, raise_on_modflow_error)
+        self.nlay = nlay
+        self.simulate_et = simulate_et
+        self.gwet_linear_or_square = gwet_linear_or_square
+        self.unsat_et_wc_or_ae = unsat_et_wc_or_ae
+
+    def get_init_parameters(self, name: str) -> DataFrame:
+        parameters = Modflow.get_init_parameters(self, name)
+        parameters.loc[name + "_k"] = (0.0, 10.0, 1.0, True, name, "uniform")
+        parameters.loc[name + "_thtr"] = (0.0, 0.2, 0.1, True, name, "uniform")
+        parameters.loc[name + "_thts"] = (0.2, 0.4, 0.3, True, name, "uniform")
+        parameters.loc[name + "_eps"] = (3.5, 10.0, 4.0, True, name, "uniform")
+        parameters.loc[name + "_extdp"] = (0.0, 1.0, 0.5, True, name, "uniform")
+        parameters.loc[name + "_ha"] = (0.0, 0.1, 0.0, False, name, "uniform")
+        parameters.loc[name + "_hroot"] = (0.0, 0.1, 0.0, False, name, "uniform")
+        parameters.loc[name + "_rootact"] = (0.0, 0.1, 0.0, False, name, "uniform")
+        return parameters
+
+    def update_model(self, p: ArrayLike):
+        c, s, k, thtr, thts, eps, extdp, ha, hroot, rootact = p[0:9]
+
+        d = 0.0
+
+        self.remove_changing_packages()
+        self.update_sto(s=s)
+        self.update_ghb(d=d, c=c)
+
+        p = self._stress[0]
+        e = self._stress[1]  # make sure et is positive
+        # note: for specifying uzf number, use fortran indexing!
+
+        surfdep = 1e-5
+        thti = (thts - thtr) / 2  # initial water content
+        extwc = thtr  # extiction water content
+
+        # If IVERTCON is greater than zero then residual potential evapotranspiration not satisfied in the UZF cell is applied to the underlying UZF and GWF cells.
+        ivertcon = [i + 1 for i in range(self.nlay)]
+        ivertcon[-1] = -1
+
+        uzf_pkdat = [
+            [
+                i,  # iuzno
+                (i, 0, 0),  # cellid
+                1 if i == 0 else 0,  # landflag
+                ivertcon[i],  # ivertcon
+                surfdep,  # surfdep
+                k,  # vks
+                thtr,  # thtr
+                thts,  # thts
+                thti,  # thti
+                eps,  # eps
+                f"uzf_{i:02d}",  # boundname
+            ]
+            for i in range(self.nlay)
+        ]
+
+        uzf_spd = {}
+        for iper, (ir, er) in enumerate(zip(p, e)):
+            data = [
+                [i, ir, er, extdp, extwc, ha, hroot, rootact] for i in range(self.nlay)
+            ]
+            uzf_spd[iper] = data
+        # uzf_spd = dict([(i, []) for i, (ir, er) in enumerate(zip(p, e))])
+
+        linear_gwet = self.gwet_linear_or_square == "linear"
+        unsat_etae = self.unsat_et_wc_or_ae == "ae"
+        _ = flopy.mf6.ModflowGwfuzf(
+            self._gwf,
+            print_input=True,
+            print_flows=True,
+            save_flows=True,
+            boundnames=True,
+            simulate_et=True,  # If this option is selected, evapotranspiration will be simulated in the unsaturated zone but not in the saturated zone.
+            linear_gwet=linear_gwet,  # If this option is selected, evapotranspiration will be simulated in both the unsaturated and saturated zones. The groundwater evapotranspiration will be simulated using the original ET formulation of MODFLOW-2005.
+            square_gwet=not linear_gwet,  # If this option is selected, evapotranspiration will be simulated in both the unsaturated and saturated zones. The groundwater evapotranspiration will be simulated by assuming a constant evapotranspiration rate for groundwater levels between land surface (TOP) and land surface minus the evapotranspiration extinction depth (TOP-EXTDP). Groundwater evapotranspiration is smoothly reduced from the potential evapotranspiration rate to zero over a nominal interval at TOP-EXTDP.
+            unsat_etwc=not unsat_etae,  # Evapotranspiration in the unsaturated zone will be simulated as a function of the specified potential evapotranspiration rate while the water content (THETA) is greater than the ET extinction water content (EXTWC).
+            unsat_etae=unsat_etae,  # Evapotranspiration in the unsaturated zone will be simulated simulated using a capillary pressure based formulation. Capillary pressure is calculated using the Brooks-Corey retention function.
+            simulate_gwseep=True,
+            ntrailwaves=15,
+            nwavesets=75,
+            nuzfcells=len(uzf_pkdat),
+            packagedata=uzf_pkdat,
+            perioddata=uzf_spd,
+            budget_filerecord=f"{self._name}.uzf.bud",
+            wc_filerecord=f"{self._name}.uzf.bin",
+            # observations=uzf_obs,
+            pname="uzf",
+            filename=f"{self._name}.uzf",
+        )
         self._gwf.name_file.write()
