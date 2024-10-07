0b5b37b14ef86d17e3c1378d113599517d27c29a
Martin Vonk <vonk.mart@gmail.com>
2024-10-07 11:24:53 +0200

    add uzf to init

--- pastas_plugins/modflow/__init__.py
+++ pastas_plugins/modflow/__init__.py
@@ -1,4 +1,4 @@
 # ruff: noqa: F401
-from pastas_plugins.modflow.modflow import ModflowRch
+from pastas_plugins.modflow.modflow import ModflowRch, ModflowUzf
 from pastas_plugins.modflow.stressmodels import ModflowModel
 from pastas_plugins.modflow.version import __version__
