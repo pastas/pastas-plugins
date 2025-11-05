a7f84f2f7c858d7c1d150cf6b14124da096562c2
martinvonk <vonk.mart@gmail.com>
2025-11-05 12:03:08 +0100

    add back seeds

--- pastas_plugins/pest/solver.py
+++ pastas_plugins/pest/solver.py
@@ -658,6 +658,7 @@ class PestIesSolver(PestSolver):
         | None = None,
         pestpp_options: dict[str, Any] | None = None,
         silent: bool = False,
+        seed: int = pyemu.en.SEED,
     ) -> None:
         """
         Run ensemble simulations using pestpp-ies.
@@ -697,6 +698,7 @@ class PestIesSolver(PestSolver):
             self.write_ensemble_observation_noise(
                 standard_deviation=observation_noise_standard_deviation,
                 correlation_coefficient=observation_noise_correlation_coefficient,
+                seed=seed,
             )
             pst.pestpp_options["ies_observation_ensemble"] = (
                 "pest_starting_obs_ensemble.csv"
@@ -706,6 +708,7 @@ class PestIesSolver(PestSolver):
                 method=ies_parameter_ensemble_method,
                 par_sigma_range=par_sigma_range,
                 ies_add_base=ies_add_base,
+                seed=seed,
             )
             pst.pestpp_options["ies_parameter_ensemble"] = (
                 "pest_starting_par_ensemble.csv"
@@ -906,6 +909,7 @@ class PestIesSolver(PestSolver):
         standard_deviation: float = 0.0,
         correlation_coefficient: float = 0.0,
         ies_add_base: bool = True,
+        seed: int = pyemu.en.SEED,
     ) -> None:
         """
         Generate and write an ensemble of observation noise to a CSV file.
@@ -931,7 +935,7 @@ class PestIesSolver(PestSolver):
             nobs=len(pst.observation_data.index),
             standard_deviation=standard_deviation,
             correlation_coefficient=correlation_coefficient,
-            seed=pyemu.en.SEED,
+            seed=seed,
         )
         obs_data = pst.observation_data.loc[:, ["obsval"]].values
         obs_noise_df = pd.DataFrame(
