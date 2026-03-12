e0dea0ec851b7168a61f2f66d52903ec2c5d06fa
Ruben Caljé <r.calje@artesia-water.nl>
2026-03-12 09:24:36 +0100

    simplify the init, to make it less dependent on chnages of WellModel

--- pastas_plugins/responses/stressmodels.py
+++ pastas_plugins/responses/stressmodels.py
@@ -30,77 +30,18 @@ class WellModel2(WellModel):
 
     _name = "WellModel2"
 
-    def __init__(
-        self,
-        stress: list[Series],
-        name: str,
-        distances: ArrayLike,
-        rfunc: RFunc | None = None,
-        up: bool = False,
-        settings: str | StressSettingsDict = "well",
-        sort_wells: bool = True,
-        metadata: list[dict[str, Any]] = None,
-        max_cache_size: int = None,
-    ) -> None:
+    def __init__(self, *args, rfunc: RFunc | None = None, **kwargs) -> None:
+        # call super init to set all properties the same as WellModel, except for rfunc
+        super().__init__(*args, **kwargs)
         # check response function
         if rfunc is None:
             rfunc = Hantush()
         elif not isinstance(rfunc, Hantush):
             raise NotImplementedError("WellModel2 only supports the rfunc Hantush!")
-
-        # check if number of stresses and distances match
-        if len(stress) != len(distances):
-            msg = (
-                "The number of stresses does not match the number of distances "
-                "provided."
-            )
-            logger.error(msg)
-            raise ValueError(msg)
-        else:
-            self.distances = Series(
-                index=[s.squeeze().name for s in stress],
-                data=distances,
-                name="distances",
-            )
-
-        # parse settings input
-        if settings is None or isinstance(settings, str) or isinstance(settings, dict):
-            settings = len(stress) * [settings]
-
-        # if metadata is passed as dict -> convert to list
-        if metadata is not None and isinstance(metadata, dict):
-            metadata = [metadata]
-
-        # parse stresses input
-        stress = self._handle_stress(stress, settings, metadata)
-
-        # sort wells by distance
-        self.sort_wells = sort_wells
-        if self.sort_wells:
-            stress = [
-                s for _, s in sorted(zip(distances, stress), key=lambda pair: pair[0])
-            ]
-            self.distances.sort_values(inplace=True)
-
-        # estimate gain_scale_factor w/ max of stresses stdev
-        gain_scale_factor = np.max([s.series.std() for s in stress])
-
-        tmin = np.min([s.series.index.min() for s in stress])
-        tmax = np.max([s.series.index.max() for s in stress])
-
-        StressModelBase.__init__(
-            self,
-            name=name,
-            tmin=tmin,
-            tmax=tmax,
-            rfunc=rfunc,
-            up=up,
-            gain_scale_factor=gain_scale_factor,
-            max_cache_size=max_cache_size,
-        )
-
-        self.stress = stress
-        self.freq = self.stress[0].settings["freq"]
+        up = self.rfunc.up
+        gain_scale_factor = self.rfunc.gain_scale_factor
+        rfunc.update_rfunc_settings(up=up, gain_scale_factor=gain_scale_factor)
+        self.rfunc = rfunc
         self.set_init_parameters()
 
     def set_init_parameters(self) -> None:
