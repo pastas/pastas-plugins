33dc709dcfa3eb7580a49f7713cf34b3894179ab
martinvonk <vonk.mart@gmail.com>
2026-05-13 14:11:46 +0200

    do the same for the other plugins

--- pastas_plugins/responses/__init__.py
+++ pastas_plugins/responses/__init__.py
@@ -1,3 +1,9 @@
 # ruff : noqa: F401
+# ruff : noqa: F401
+import pastas as ps
+
 from pastas_plugins.responses.rfunc import Edelman, Theis
 from pastas_plugins.responses.version import __version__
+
+ps.solver.Edelman = Edelman
+ps.solver.Theis = Theis
